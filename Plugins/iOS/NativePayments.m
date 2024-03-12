719bad18c085d9808974c4e32fa97ba9531c09eb
Ivan Klimchuk <alroniks@gmail.com>
2024-03-12 23:31:29 +0300

    Update native code of native plugin as it works fine

--- Plugins/iOS/NativePayments.m
+++ Plugins/iOS/NativePayments.m
@@ -4,35 +4,49 @@
 typedef void (*successCallbackDelegate)(const char* data);
 typedef void (*failureCallbackDelegate)(const char* code, const char* message);
 
-extern UIViewController *UnityGetGLViewController();
+//typedef void (*ddd)(const bool* status, const char* message);
 
 static NSString* stringFromChar(const char *string) {
     return string ? [NSString stringWithUTF8String:string] : nil;
 }
 
-@interface NativePayments : NSObject<PKPaymentAuthorizationViewControllerDelegate>
+@interface NativePayments : NSObject<PKPaymentAuthorizationControllerDelegate>
 
-@property (nonatomic, strong) PKPaymentAuthorizationViewController * _Nullable viewController;
+@property (nonatomic, strong) PKPaymentAuthorizationController * _Nullable viewController;
 @property (nonatomic, copy) void (^__strong _Nonnull completion)(PKPaymentAuthorizationResult * _Nonnull __strong);
 @property (nonatomic) successCallbackDelegate _Nullable success;
 @property (nonatomic) failureCallbackDelegate _Nullable failure;
 
+//handler:(void (^)(PKPaymentAuthorizationResult *result))completion
+
 @end
+
 
 @implementation NativePayments
 
 static const PKMerchantCapability PKMerchantCapabilityUnknown = 9999;
 static const PKPaymentNetwork PKPaymentNetworkUnknown = 0;
 
++ (instancetype) sharedInstance
+{
+    static id sharedInstance = nil;
+
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+
+    return sharedInstance;
+}
+
 - (void) showPaymentsView: (NSString *) params
-                   success: (successCallbackDelegate) success
-                   failure: (failureCallbackDelegate) failure
+                  success: (successCallbackDelegate) success
+                  failure: (failureCallbackDelegate) failure
 {
     self.success = success;
     self.failure = failure;
 
     NSError *error;
-
     NSData *json = [params dataUsingEncoding:NSUTF8StringEncoding];
     NSDictionary *data = [NSJSONSerialization JSONObjectWithData:json options:kNilOptions error:&error];
 
@@ -92,41 +106,24 @@ static const PKPaymentNetwork PKPaymentNetworkUnknown = 0;
     paymentRequest.paymentSummaryItems = paymentSummaryItems;
 
     if (data[@"RequiredBillingContactFields"]) {
+        // todo: need apply mapping and configure from server?
         paymentRequest.requiredBillingContactFields = [NSSet setWithArray:@[PKContactFieldName, PKContactFieldEmailAddress, PKContactFieldPostalAddress, PKContactFieldPhoneNumber]];
     }
 
-    self.viewController = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:paymentRequest];
-    [self.viewController setDelegate:self];
+    self.viewController = [[PKPaymentAuthorizationController alloc] initWithPaymentRequest:paymentRequest];
+    self.viewController.delegate = self;
 
     if (!self.viewController) {
-        self.failure([@"no_view_controller" UTF8String], [@"Failed initializing PKPaymentAuthorizationViewController, check you app ApplePay capabilities and merchantIdentifier" UTF8String]);
+        failure([@"no_view_controller" UTF8String], [@"Failed initializing PKPaymentAuthorizationViewController, check you app ApplePay capabilities and merchantIdentifier" UTF8String]);
         return;
     }
 
-    NSLog(@"can make? %d", [PKPaymentAuthorizationViewController canMakePayments]);
-
-//    [UnityGetGLViewController().view addSubview:self.viewController.view];
-
-//    [self.viewController presentViewController:self.viewController animated:YES completion:nil];
-
-//    [UnityGetGLViewController() presentViewController:self.viewController animated:YES completion:^{
-//        self.success([@"showed!!!" UTF8String]);
-//        // ???
-//    }];
-
-    [UnityGetGLViewController() showViewController:self.viewController sender:nil];
-
-//    [UnityGetGLViewController() presentViewController:self.viewController animated:YES completion:^{
-//        self.success([@"displayed!!!" UTF8String]);
-//    }];
-}
-
-- (void) abort
-{
-    NSLog(@"aboter");
-//    [UnityGetGLViewController() dismissViewControllerAnimated:YES completion:^{
-//        self.success([@"aborted" UTF8String]);
-//    }];
+    [self.viewController presentWithCompletion:^(BOOL success) {
+        if (success) {
+            NSLog(@"Payment shit is presented to the player");
+            self.success([@"Payment shit is presented to the player" UTF8String]);
+        }
+    }];
 }
 
 - (void) complete: (NSString *) incomingStatus
@@ -144,86 +141,71 @@ static const PKPaymentNetwork PKPaymentNetworkUnknown = 0;
     self.success("ololo completed");
 }
 
-- (void) paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller
-                        didAuthorizePayment:(PKPayment *)payment
-                                    handler:(void (^)(PKPaymentAuthorizationResult *result))completion
+- (void) paymentAuthorizationController:(PKPaymentAuthorizationViewController *)controller
+                    didAuthorizePayment:(PKPayment *)payment
+                                handler:(void (^)(PKPaymentAuthorizationResult *result))completion
 {
-//    controller.presentedViewController = UnityGetGLViewController();
+    self.completion = completion;
 
     NSLog(@"Seems comething triggered here!");
 
-    self.completion = completion;
+    NSMutableDictionary *paymentDict = [NSMutableDictionary dictionary];
+    NSMutableDictionary *tokenDict = [NSMutableDictionary dictionary];
+    tokenDict[@"transactionIdentifier"] = payment.token.transactionIdentifier;
 
-//    [controller dismissViewControllerAnimated:YES completion:^{
-//        NSLog(@"disnussed");
-//    }];
+    NSString *paymentData64 = [payment.token.paymentData base64EncodedStringWithOptions:0];
+    NSData *decodedPaymentData = [[NSData alloc] initWithBase64EncodedString:paymentData64 options:0];
+    tokenDict[@"paymentData"] = [[NSString alloc] initWithData:decodedPaymentData encoding:NSUTF8StringEncoding];
 
-//    NSMutableDictionary *paymentDict = [NSMutableDictionary dictionary];
-//
-//    NSMutableDictionary *tokenDict = [NSMutableDictionary dictionary];
-//    tokenDict[@"transactionIdentifier"] = payment.token.transactionIdentifier;
-//
-//    NSString *paymentData64 = [payment.token.paymentData base64EncodedStringWithOptions:0];
-//    NSData *decodedPaymentData = [[NSData alloc] initWithBase64EncodedString:paymentData64 options:0];
-//    tokenDict[@"paymentData"] = [[NSString alloc] initWithData:decodedPaymentData encoding:NSUTF8StringEncoding];
-//
-//    NSMutableDictionary *paymentMethodDict = [NSMutableDictionary dictionary];
-//    paymentMethodDict[@"displayName"] = payment.token.paymentMethod.displayName;
-//    paymentMethodDict[@"network"] = payment.token.paymentMethod.network;
-//    paymentMethodDict[@"type"] = [self stringFromPaymentMethodType:payment.token.paymentMethod.type];
-//
-//    tokenDict[@"paymentMethod"] = paymentMethodDict;
-//
-//    paymentDict[@"token"] = tokenDict;
+    NSMutableDictionary *paymentMethodDict = [NSMutableDictionary dictionary];
+    paymentMethodDict[@"displayName"] = payment.token.paymentMethod.displayName;
+    paymentMethodDict[@"network"] = payment.token.paymentMethod.network;
+    paymentMethodDict[@"type"] = [self stringFromPaymentMethodType:payment.token.paymentMethod.type];
 
-//    PKContact *billingContact = payment.billingContact;
-//    if (billingContact) {
-//        NSMutableDictionary *billingContactDict = [NSMutableDictionary dictionary];
-//
-//        CNPostalAddress *postalAddress = billingContact.postalAddress;
-//        NSMutableDictionary *postalAddressDict = [NSMutableDictionary dictionary];
-//        if (postalAddress) {
-//            postalAddressDict[@"street"] = postalAddress.street;
-//            postalAddressDict[@"city"] = postalAddress.city;
-//            postalAddressDict[@"state"] = postalAddress.state;
-//            postalAddressDict[@"postalCode"] = postalAddress.postalCode;
-//            postalAddressDict[@"country"] = postalAddress.country;
-//            postalAddressDict[@"isoCountryCode"] = postalAddress.ISOCountryCode;
-//        }
-//
-//        billingContactDict[@"postalAddress"] = postalAddressDict;
-//
-//        paymentDict[@"billingContact"] = billingContactDict;
-//    }
+    tokenDict[@"paymentMethod"] = paymentMethodDict;
+    paymentDict[@"token"] = tokenDict;
 
-//    NSError *error;
-//
-//    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:paymentDict options:NSJSONWritingPrettyPrinted error:&error];
-//
-//    if (!jsonData) {
-//        self.failure([@"json_serialization_error" UTF8String], [@"Failed to serialize PKPayment to JSON." UTF8String]);
-//    } else {
-//        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-//        self.success([jsonString UTF8String]);
-//    }
+    PKContact *billingContact = payment.billingContact;
+    if (billingContact) {
+        NSMutableDictionary *billingContactDict = [NSMutableDictionary dictionary];
 
-    self.success([@"Seems catched it!!! Payment is good" UTF8String]);
+        CNPostalAddress *postalAddress = billingContact.postalAddress;
+        NSMutableDictionary *postalAddressDict = [NSMutableDictionary dictionary];
+        if (postalAddress) {
+            postalAddressDict[@"street"] = postalAddress.street;
+            postalAddressDict[@"city"] = postalAddress.city;
+            postalAddressDict[@"state"] = postalAddress.state;
+            postalAddressDict[@"postalCode"] = postalAddress.postalCode;
+            postalAddressDict[@"country"] = postalAddress.country;
+            postalAddressDict[@"isoCountryCode"] = postalAddress.ISOCountryCode;
+        }
 
-//    self.success = nil;
-//    self.failure = nil;
+        billingContactDict[@"postalAddress"] = postalAddressDict;
+
+        paymentDict[@"billingContact"] = billingContactDict;
+    }
+
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:paymentDict options:NSJSONWritingPrettyPrinted error:&error];
+
+    if (!jsonData) {
+        self.failure([@"json_serialization_error" UTF8String], [@"Failed to serialize PKPayment to JSON." UTF8String]);
+    } else {
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        self.success([jsonString UTF8String]);
+    }
+
+    PKPaymentAuthorizationResult *result = [[PKPaymentAuthorizationResult alloc] initWithStatus:PKPaymentAuthorizationStatusFailure errors:nil];
+
+    completion(result);
 }
 
-- (void) paymentAuthorizationViewControllerDidFinish:(PKPaymentAuthorizationViewController *)controller
+- (void) paymentAuthorizationControllerDidFinish:(PKPaymentAuthorizationController *)controller
 {
-    NSLog(@"I am going to finish it!");
-
-    [controller dismissViewControllerAnimated:YES completion:^{
-        self.failure([@"payment_error" UTF8String], [@"Payment 1 process canceled by user." UTF8String]);
+    [controller dismissWithCompletion:^{
+        NSLog(@"Payment process canceled by user.");
+        self.failure([@"payment_error" UTF8String], [@"Payment process canceled by user." UTF8String]);
     }];
-
-//    [UnityGetGLViewController() dismissViewControllerAnimated:YES completion:^{
-//        self.failure([@"payment_error" UTF8String], [@"Payment 2 process canceled by user." UTF8String]);
-//    }];
 }
 
 - (PKPaymentNetwork)paymentNetworkFromString:(NSString *)paymentNetworkString {
@@ -275,13 +257,20 @@ static const PKPaymentNetwork PKPaymentNetworkUnknown = 0;
 
 @end
 
-
 extern void _showPaymentsView(const char* config, successCallbackDelegate success, failureCallbackDelegate failure) {
-    NativePayments *payments = [[NativePayments alloc] init];
-    [payments showPaymentsView:stringFromChar(config) success:success failure:failure];
+    [[NativePayments sharedInstance] showPaymentsView:stringFromChar(config) success:success failure:failure];
 }
 
-bool _canMakePayments()
+//extern void _putConfirmation()
+//{
+//    [[NativePayments sharedInstance] complete:YES success:<#(successCallbackDelegate)#> failure:<#(failureCallbackDelegate)#>];
+//}
+
+//_canMakePayments()
+//_askPaymentSheet()
+//_putConfirmation()
+
+extern bool _canMakePayments()
 {
     return [PKPaymentAuthorizationViewController canMakePayments];
 }
