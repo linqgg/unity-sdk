a29c61d1c63f89e3c0a85511b29bb969992f0bfb
Ivan Klimchuk <alroniks@gmail.com>
2024-03-13 19:58:42 +0300

    Add postbuild script for registering apple pay capabilities and merchant ids

---
+++ Editor/PostBuildScript.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+
+public class PostBuildIosScript
+{
+  [PostProcessBuild(1)]
+  public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+  {
+#if UNITY_IOS
+    Debug.Log("Initializing ApplePay capabilities for LinQ Unity SDK");
+
+    var path = PBXProject.GetPBXProjectPath(pathToBuiltProject);
+    PBXProject project = new PBXProject();
+    project.ReadFromString(File.ReadAllText(path));
+
+    string targetGuid = project.GetUnityMainTargetGuid();
+
+    project.AddCapability(targetGuid, PBXCapabilityType.ApplePay);
+
+    File.WriteAllText(path, project.WriteToString());
+
+    var manager = new ProjectCapabilityManager(path, "Entitlements.entitlements", null, targetGuid);
+
+    manager.AddApplePay(new string[]
+    {
+      "merchant.games.galactica.linq-test",
+      "merchant.games.galactica.linq-2-test",
+      "merchant.games.galactica.linq",
+      "merchant.games.galactica.linq-2",
+    });
+
+    manager.WriteToFile();
+#endif
+  }
+}
