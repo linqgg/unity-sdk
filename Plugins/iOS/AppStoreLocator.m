a71aab60e33e1ce07a2bdd4d846fb026ca151628
Ivan Klimchuk <alroniks@gmail.com>
2024-06-16 22:37:04 +0300

    Move custom plugins into SDK

---
+++ Plugins/iOS/AppStoreLocator.m
@@ -0,0 +1,28 @@
+#import <StoreKit/StoreKit.h>
+
+char* convert(const NSString* string)
+{
+    if (string == NULL) {
+        return NULL;
+    }
+    
+    const char* encoded = [string UTF8String];
+    char* result = (char*) malloc(strlen(encoded) + 1);
+    strcpy(result, encoded);
+    
+    return result;
+}
+
+char* _GetAppStoreRegion () {
+    
+    NSString* country = @"USA";
+    
+    if (@available(iOS 13.0, *)) {
+        NSString* code = [[[SKPaymentQueue defaultQueue] storefront] countryCode];
+        if (code) {
+            country = code;
+        }
+    }
+    
+    return convert(country);
+}
