d40c2068f2eccdfa26a4dacc5cab51182b1f16e5
Ivan Klimchuk <alroniks@gmail.com>
2024-09-30 11:25:35 +0300

    Remove fallback to USA country in app store country locator (#28)

--- Plugins/iOS/AppStoreLocator.m
+++ Plugins/iOS/AppStoreLocator.m
@@ -5,24 +5,24 @@ char* convert(const NSString* string)
     if (string == NULL) {
         return NULL;
     }
-    
+
     const char* encoded = [string UTF8String];
     char* result = (char*) malloc(strlen(encoded) + 1);
     strcpy(result, encoded);
-    
+
     return result;
 }
 
 char* _GetAppStoreRegion () {
-    
-    NSString* country = @"USA";
-    
+
+    NSString* country = @"";
+
     if (@available(iOS 13.0, *)) {
         NSString* code = [[[SKPaymentQueue defaultQueue] storefront] countryCode];
         if (code) {
             country = code;
         }
     }
-    
+
     return convert(country);
 }
